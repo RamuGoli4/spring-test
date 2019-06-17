0252f30d14415c261c4534696450565659b35d28
RP <ramu.goli4@gmail.com>
2019-06-17 22:51:20 +0530

    Initial Code

---
+++ src/main/java/com/spring/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String tst() {
+        System.out.println("In str");
+        return "Hello String";
+    }
+}
